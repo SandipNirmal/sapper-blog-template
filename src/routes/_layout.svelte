0c451478fcdb87b5b009ed92ac0c1e17d7ff66e5
Sandip Nirmal <sandipnirmal@icloud.com>
2020-10-10 17:26:26 +0530

    Initial commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,54 @@
+<script>
+  import Header from '../components/Header.svelte';
+
+  export let segment;
+</script>
+
+<style>
+  .layout {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 1440px;
+    margin: 0 auto;
+    min-height: 100%;
+    min-height: 100vh;
+  }
+
+  main {
+    flex: 1;
+    position: relative;
+    margin: 0 auto;
+    max-width: 1280px;
+    background-color: white;
+    padding: 1em 2em;
+    box-sizing: border-box;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  footer {
+    color: #aaa;
+    font-size: 1em;
+    margin: 1em auto;
+    padding: 1em 2em;
+    text-align: center;
+    width: 100%;
+  }
+</style>
+
+<div class="layout">
+  <Header {segment} />
+
+  <main>
+    <slot />
+  </main>
+
+  <footer>
+    <span>
+      By: Sandip Nirmal.
+      Made with <a href="https://sapper.svelte.dev" target="_blank">Sapper</a>.
+    </span>
+  </footer>
+</div>
---
+++ src/routes/blog/posts/hello-world.md
@@ -0,0 +1,17 @@
+---
+title: नमस्कार 👏
+date: "2019-06-11T08:38:00.000Z"
+---
+
+Every blog starts with a single post. This is yours. Make it great.
+
+<!-- more -->
+
+This post intentionally left blank.
+
+Write what you want.
+
+```javascript
+function getUsername(user) {
+  return user.name;
+}
