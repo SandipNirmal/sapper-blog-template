3bf9dc76bef73460f59ca537bc7f279195a74fb8
Sandip Nirmal <sandipnirmal@icloud.com>
2020-10-11 21:26:44 +0530

    Added support for dark mode

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,14 +1,37 @@
 <script>
   export let segment;
+
+  let isDarkMode = false;
+
+  if (process.browser) {
+    const theme = localStorage.getItem('theme');
+
+    if (theme) {
+      isDarkMode = theme === 'dark';
+    } else {
+      isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    }
+
+    const page = document.querySelector('html');
+    page.setAttribute('data-theme', isDarkMode ? 'dark' : '');
+  }
+
+  const changeTheme = () => {
+    if (process.browser) {
+      const page = document.querySelector('html');
+      isDarkMode = !isDarkMode;
+      page.setAttribute('data-theme', isDarkMode ? 'dark' : '');
+      localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+    }
+  };
 </script>
 
 <style>
   nav {
-    align-items: flex-end;
+    align-items: center;
     display: flex;
     flex: 1;
     justify-content: flex-end;
-    /* font-size: 14px; */
   }
 
   a {
@@ -29,9 +52,26 @@
   a:not(.selected) {
     opacity: 0.88;
   }
+
+  button {
+    background: transparent;
+    text-align: center;
+    font-size: 16px;
+    cursor: pointer;
+    outline: none;
+  }
+
+  @media (max-width: 480px) {
+    a {
+      margin-left: 8px;
+    }
+  }
 </style>
 
 <nav>
+  <button
+    title={isDarkMode ? 'Turn on the Light' : 'Dim the Light'}
+    on:click={changeTheme}>{isDarkMode ? '☀️' : '🌒'}</button>
   <a class={segment === 'about' ? 'selected' : ''} href="about">About</a>
   <a
     rel="prefetch"
